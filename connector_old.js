c888e8d0d232f95e4fa05f8d20a47aa699cd1db1
Amit Zur <sendwithchibo@gmail.com>
2019-01-17 07:38:12 +0200

    connector_old

---
+++ connector_old.js
@@ -0,0 +1,30 @@
+const {ServerConnector, PromiseFactory, Logger, ConsoleLogHandler} = require('eyes.sdk');
+const url = require('url');
+
+const serverUrl = process.argv[2];
+const apiKey = process.argv[3];
+const proxy = process.argv[4];
+
+let proxyToUse = (proxy && proxy !== 'false') ? proxy : false;
+
+console.log('serverUrl:', serverUrl);
+console.log('apiKey:', apiKey);
+console.log('proxy:', proxyToUse === false ? 'no proxy' : proxy);
+
+const logger = new Logger();
+logger.setLogHandler(new ConsoleLogHandler(true));
+const serverConnector = new ServerConnector(new PromiseFactory(), serverUrl, logger);
+serverConnector.setApiKey(apiKey);
+serverConnector.setProxy(proxyToUse);
+
+(async function() {
+  try {
+    const res = await serverConnector.startSession({
+      agentId: 'some_agentid',
+      appIdOrName: 'some_app'
+    });
+    console.log('res', res);
+  } catch(err) {
+    console.log('err', err);
+  }
+})();
